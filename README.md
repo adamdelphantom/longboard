03b1b29d11042cd583252e5df72c44757fdc896b
Adam Del Sol <adamdelsolrun@gmail.com>
2022-07-31 11:24:46 -0400

    begin to layout client for swap program and abstracted some calls to functions

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# The Plan
+# Longboard Client TODOs
 
 - create solana wallet: DONE
 - output pubkey: DONE
 - sign and send txn: DONE
+
+### Client actions for simple swap program
+
+program: https://github.com/adamdelsol/adamdelsol_swap
+
+- create wallets in env for permanence
 - create spl-token
 - mint token
 - transfer token
+
+### Low priority
+
+- handle async wallet ops better
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
-use solana_client::rpc_client::RpcClient;
-use solana_sdk::{signature::{Keypair, Signer, Signature}, pubkey::Pubkey, system_transaction};
+use solana_client::{rpc_client::RpcClient};
+use solana_sdk::{signature::{Keypair, Signer, Signature}, pubkey::Pubkey, system_transaction, lamports};
 
 fn main() {
     let tenth_sol_lamps = 100000000;
@@ -11,10 +11,11 @@ fn main() {
     let bob_wallet = Keypair::new();
 
     println!("Alice: {:?}", alice_wallet.pubkey());
+    println!("Alice's privkey: {:?}", alice_wallet.secret());
     println!("Bob: {:?}", bob_wallet.pubkey());
 
-    let alice_airdrop_request_sig = request_airdrop(&rpc_client, &alice_wallet.pubkey(), tenth_sol_lamps).expect("alice's airdrop request failed");
-    let bob_airdrop_request_sig = request_airdrop(&rpc_client, &bob_wallet.pubkey(), tenth_sol_lamps).expect("bob's airdrop request failed");
+    let alice_airdrop_request_sig = request_one_sol(&rpc_client, &alice_wallet.pubkey()).expect("alice's airdrop request failed");
+    let bob_airdrop_request_sig = request_one_sol(&rpc_client, &bob_wallet.pubkey()).expect("bob's airdrop request failed");
 
     println!("Alice airdrop sig: {:?}", alice_airdrop_request_sig);
     println!("Bob airdrop sig: {:?}", bob_airdrop_request_sig);
@@ -22,21 +23,15 @@ fn main() {
     println!("Alice's balance: {:?}", rpc_client.get_balance(&alice_wallet.pubkey()).expect("Could not get Alice's balance."));
     println!("Bob's balance: {:?}", rpc_client.get_balance(&bob_wallet.pubkey()).expect("Could not get Bob's balance."));
 
-    rpc_client.send_and_confirm_transaction(&system_transaction::transfer(
-        &alice_wallet,
-        &bob_wallet.pubkey(),
-        tenth_sol_lamps/2,
-        rpc_client.get_latest_blockhash().unwrap()
-    ));
-
+    transfer_sol(&rpc_client, &alice_wallet, &bob_wallet.pubkey(), tenth_sol_lamps);
     
     println!("Alice's balance: {:?}", rpc_client.get_balance(&alice_wallet.pubkey()).expect("Could not get Alice's balance."));
     println!("Bob's balance: {:?}", rpc_client.get_balance(&bob_wallet.pubkey()).expect("Could not get Bob's balance."));
 
 }
 
-pub fn request_airdrop(rpc_client: &RpcClient, pub_key: &Pubkey, amount_sol: u64) -> Result<Signature, Box<dyn Error>> {
-    let sig = rpc_client.request_airdrop(pub_key, amount_sol)?;
+pub fn request_one_sol(rpc_client: &RpcClient, pub_key: &Pubkey) -> Result<Signature, Box<dyn Error>> {
+    let sig = rpc_client.request_airdrop(pub_key, 1000000000)?;
     loop {
         let confirmed = rpc_client.confirm_transaction(&sig)?;
         if confirmed {
@@ -44,4 +39,15 @@ pub fn request_airdrop(rpc_client: &RpcClient, pub_key: &Pubkey, amount_sol: u64
         }
     }
     Ok(sig)
+}
+
+pub fn transfer_sol(client: &RpcClient, 
+    from_keypair: &Keypair, 
+    to_pubkey: &Pubkey, 
+    lamports_to_send: u64) -> Result<Signature, solana_client::client_error::ClientError>{
+    client.send_and_confirm_transaction(&system_transaction::transfer(
+                from_keypair, 
+                to_pubkey, 
+                lamports_to_send, 
+                client.get_latest_blockhash().unwrap()))
 }
